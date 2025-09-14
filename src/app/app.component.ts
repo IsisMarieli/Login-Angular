db2e88ee62ab488bb22dd0a382d00badc2cf0ee3
IsisMarieli <Isismarieli1@gmail.com>
2025-09-14 00:25:21 -0300

    criando os primeiros components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LinkButtonComponent } from './components/link-button/link-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LinkButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/link-button/link-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[link-button]',
+  imports: [],
+  template: '<ng-content/>',
+  styleUrl: './link-button.component.scss'
+})
+export class LinkButtonComponent {
+
+}
